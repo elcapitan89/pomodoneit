0c3552ed67bd72023742cce3b04c5493ec4f1c3d
Will Rittwage <wbrittwage@protonmail.com>
2021-08-05 14:05:07 -0700

    timer working correctly

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
-let color = '#3aa757';
+let workMins = 25;
+let breakMins = 5;
+let deadline = null;
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  chrome.storage.sync.set({ breakMins });
+  chrome.storage.sync.set({ workMins });
+  // chrome.storage.sync.set({ deadline });
+
+  console.log(`Default work time set to ${workMins} mins and break time set to ${breakMins} mins`);
+  chrome.storage.sync.get(['workMins', 'breakMins'], function (result) {
+    console.log(result.workMins);
+    console.log(result.breakMins);
+  })
 });
--- popup.js
+++ popup.js
@@ -1,23 +1,65 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+// zero out the coundown clock
+let countdown = document.getElementById('countdown');
+countdown.innerText = '0m0s';
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+let workMins;
+let breakMins;
+chrome.storage.sync.get(['workMins', 'breakMins'], function (result) {
+  workMins = result.workMins;
+  breakMins = result.breakMins;
+
+})
+
+// listen for button click, start timer
+startPomodoro.addEventListener("click", async () => {
+
+  let currDate = new Date();
+  let currDateInMs = currDate.getTime();
+  let addMlSeconds = workMins * 60000; // TODO get mins from storage
+  let workMinsFromNow = new Date(currDateInMs + addMlSeconds);
+
+  // window.alert('foo');
+
+  // let node = document.createElement('p')
+  // node.innerText = 'START THE TIMER!';
+  // document.body.appendChild(node);
+
+  let workInterval = setInterval(() => {
+    document.getElementById('status').innerHTML = "WORKING";
+
+
+    let now = new Date().getTime();
+    let distance = workMinsFromNow - now;
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown").innerText = minutes + "m" + seconds + "s";  //this is the output on screen. ID = "demo"
+
+    if (distance < 0) {  //when countdown is up, tells you to take a break
+      clearInterval(workInterval)
+      document.getElementById("countdown").innerHTML = "GREAT JOB! TAKE A BREAK!"
+
+      let currDate = new Date();
+      let currDateInMs = currDate.getTime();
+      let addMlSeconds = breakMins * 60000; // TODO get mins from storage
+      let breakMinsFromNow = new Date(currDateInMs + addMlSeconds);
+
+      let breakInterval = setInterval(() => {
+        document.getElementById('status').innerHTML = "BREAK TIME";
+
+        let now = new Date().getTime();
+        let distance = breakMinsFromNow - now;
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById("countdown").innerText = minutes + "m" + seconds + "s";  //this is the output on screen. ID = "demo"
+
+        if (distance < 0) {  //when countdown is up, tells you to take a break
+          clearInterval(breakInterval)
+          document.getElementById("countdown").innerHTML = "BREAKS OVER!"
+        }
+      }, 100);
+    }
+  }, 100);
 });
 
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+
+
